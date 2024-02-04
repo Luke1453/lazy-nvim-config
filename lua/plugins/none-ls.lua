efd19ec9a062f0fccb69f4f01fc8c05406d66c1e
Luke1453 <lukjut@gmail.com>
2024-02-04 13:03:39 +0200

    go plugins + terminal ESC keymap

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,33 @@
+return {
+  "nvimtools/none-ls.nvim",
+  opts = function(_, opts)
+    local nls = require("null-ls")
+    opts.root_dir = opts.root_dir
+      or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+    opts.sources = vim.list_extend(opts.sources or {}, {
+      nls.builtins.formatting.fish_indent,
+      nls.builtins.diagnostics.fish,
+      nls.builtins.formatting.stylua,
+      nls.builtins.formatting.shfmt,
+      nls.builtins.formatting.gofmt,
+      nls.builtins.formatting.goimports,
+      nls.builtins.formatting.golines,
+    })
+  end,
+  on_attach = function(client, bufnr)
+    local augroup = vim.api.nvim_create_augroup("lspFormatting", {})
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+}
